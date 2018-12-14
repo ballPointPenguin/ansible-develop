eba1fd3075788c7f45093115925ec7bd45ca1b04
Benjamin Rosas <ben@aliencyb.org>
2018-12-14 03:59:17 -0600

    new setup for opensuse

---
+++ opensuse/files/alias.zsh
@@ -0,0 +1,4 @@
+alias gcmfp="git checkout master && git fetch -p --all && git pull"
+alias gloh="git log --oneline --decorate | head"
+alias nom="rm -rf node_modules && npm cache clean -f && npm install"
+alias yom="rm -rf node_modules && yarn cache clean && yarn"
---
+++ opensuse/files/compinstall.zsh
@@ -0,0 +1,6 @@
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/brosas/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
