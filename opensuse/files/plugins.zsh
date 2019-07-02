6b285c82887d901c2b09fa496e9390e600caa207
Benjamin Rosas <Benjamin Rosas>
2019-07-02 08:14:22 -0500

    get rid of unused plugins

--- opensuse/files/plugins.zsh
+++ opensuse/files/plugins.zsh
@@ -1,25 +1,8 @@
 plugins=(
-  cp
   docker
-  docker-compose
   git
-  man
-  node
   npm
-  npx
-  rebar
   suse
   systemd
   tmux
 )
-
-  # bundler
-  # emacs
-  # ember
-  # gem
-  # nvm
-  # rails
-  # rake
-  # rbenv
-  # redis-cli
-  # ruby
